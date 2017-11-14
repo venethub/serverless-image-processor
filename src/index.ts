ca125e5305ecd7604c6522b40893737ba3f26635
Benjamin Otto <benjamin.otto@style-systems.de>
2017-11-14 08:41:45 +0100

    updated dep fixed 500 response (should be 404)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { streamS3Object } from './S3Client';
 import { isSupportedInputMime, createPipe } from './pipes';
 import { getTransformer } from './Sharp';
 
-export const process = async (
+export const process = (
   event: any,
   context: any,
   cb: (err: any | null, response?: any) => void
@@ -16,33 +16,35 @@ export const process = async (
   const inputStream = streamS3Object(key, cb);
   const inputMime = getType(key);
 
-  if (!isSupportedInputMime(inputMime)) {
-    console.error(`Unsupported image ${key}`);
-    return cb(null, { statusCode: 500 });
-  }
+  inputStream.once('readable', async () => {
+    if (!isSupportedInputMime(inputMime)) {
+      console.error(`Unsupported image ${key}`);
+      return cb(null, { statusCode: 500 });
+    }
 
-  const { transformer, mime } = createPipe(
-    event.queryStringParameters || {},
-    inputMime,
-    getTransformer()
-  );
+    const { transformer, mime } = createPipe(
+      event.queryStringParameters || {},
+      inputMime,
+      getTransformer()
+    );
 
-  try {
-    const image = await inputStream.pipe(transformer).toBuffer();
+    try {
+      const image = await inputStream.pipe(transformer).toBuffer();
 
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Content-Type': mime
-      },
-      body: image.toString('base64'),
-      isBase64Encoded: true
-    };
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Content-Type': mime
+        },
+        body: image.toString('base64'),
+        isBase64Encoded: true
+      };
 
-    cb(null, response);
-  } catch (e) {
-    console.error(`Exception while transforming ${key}`);
-    console.error(e);
-    cb(null, { statusCode: 500 });
-  }
+      cb(null, response);
+    } catch (e) {
+      console.error(`Exception while transforming ${key}`);
+      console.error(e);
+      cb(null, { statusCode: 500 });
+    }
+  });
 };
